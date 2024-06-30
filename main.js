0d6331f4f57a89431145575f4237ca869f948f32
kifatet-n <n.kifayet17@gmail.com>
2024-06-30 16:53:30 +0400

    id-si root olana chance

--- main.js
+++ main.js
@@ -29,7 +29,6 @@
 //     alert("eded menfidir")
 // }
 
-
 //7
 // let str ="home work";
 // let hasword = str.includes("work")
@@ -72,14 +71,156 @@
 //14
 // let a = 5;
 //  let b = 7;
-// console.log(a != b); 
+// console.log(a != b);
+
+// let a = 6;
+// let ischeck = (a>0) ? "musbetdir" : "menfidir"
+// console.log(ischeck)
+// console.log("test")
+
+//let age = Number(prompt("yasinizi qeyd edin"))
+
+// if (age < 17) {
+//     console.log("yasi kicikdir")
+// } else {
+//     console.log("yasi boyukdur")
+// }
+
+//ic-ice sert
+
+// if (age > 17) {
+//     if (age < 6){
+//         console.log("usaq");
+//     } else {
+//         console.log("yeniyetme");
+//     }
+// }else {
+//     console.log("yasi boyukdur");
+// }
+
+//daha qisa if
+
+// if (age < 17 && age > 6) {
+//     console.log("yeni yetme");
+// } else if (age < 6) {
+//     console.log("usaqdir");
+// } else {
+//     console.log("boyukdur")
+// }
+
+// function getAge(year) {
+//     let  currentyear=2024
+//     let result = currentyear-year
+//     if (result<18) {
+//         return `yeniyetmedir ${result}`
+//     }
+//         return result;
+
+//     }
+// // ??? asagiya nece cagirdiq???
+
+// function createUserLi(index, name, year) {
+//     let _liItem = document.createElement("li");
+//     let age = 18
+//     _liItem.textContent = `${index} ) ${name}, doguldugu il: ${year} yasi: ${age}`
+//     return _liItem;
+
+// }
+// function createUserUL() {
+//     let _ul = document.createElement("ul")
+//     return _ul;
+// }
+// const _li1= createUserLi (1, "Aynur" , 2010);
+// const _li2 = createUserLi(2, "Aydan" , 2000);
+// let ul1 = createUserUL()
+// ul1.append(_li1)
+// ul1.append(_li2)
+
+// document.body.append(ul1);
+
+// for (let i=0; i<5; i++) {
+//     let a=`mehsul ${i} <br>`
+//     document.write(a)
+// }
+
+// Object
+
+// let person = {
+//     name: "Aytac",
+//     age: 27,
+//     city:"Baku"
+
+// };
+// person.name="Aynur"
+// console.log(person.name);
+
+// let arr=["nar", "banan", "alma" ]
+// console.log(arr[2])
+
+// let fruits = ['apple','banan','cherry'];
+// let fruit = fruits.map(fruit => fruit.length)
+// console.log(fruit);
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// let filteredCount = numbers.filter(num => num % 2 === 0).length
+// console.log(filteredCount);
+//  let fruits = ['apple', 'banana', 'cherry'];
+//  let newarr = fruits.join(',').split(', ')
+//  console.log
+
+
+// Named Functions
+// function getAge(year){
+//     const currentyear = 2024;
+//     let age = currentyear - year;
+//     return age
+// }
+//  let age = getAge(1995)
+//  console.log(age)
+
+// Anonymous Functions
+
+// let age = function(year){
+//     const currentyear = 2024;
+//     let age = currentyear - year;
+//     return age;
+// }
+// let age1 = age(1995);
+// console.log(age1);
+
+// Arrow Functions 
+// let age = (year = 1995) => {
+//     const currentyear = 2024;
+//     let age = currentyear - year;
+//     return age;
+// }
+// let age1 = age();
+// console.log(age1);
+
+
+// const basket = {
+//     apple: 10,
+//     banana: 15,
+//     orange: 20
+// };
+// for(const item in basket){
+//     console.log(item, basket.apple);
+// }
+        
+// let now = new Date();
+// console.log(now.getDate());
 
 
 
-let a = 6;
-let ischeck = (a>0) ? "musbetdir" : "menfidir"
-console.log(ischeck)
-console.log("test")
+// let fruits = ["apple", "orange", "cherry"];
+// fruits.forEach(fruit => console.log(fruit))
+let numbers = [1,8,0,9,5];
+console.log(numbers.reverse())
+
+
+ 
+ 
+
+
 
 
 
